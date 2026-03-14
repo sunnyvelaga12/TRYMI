b5415cc56e6da68a091ac4ca00d94dd3cd0e557d
Sunny Velaga <sunnythegamer8@gmail.com>
2026-03-14 21:40:18 +0530

    Fix: Restore missing useLocation import in Login component

--- frontend/src/frontend/components/TRYMI_login.jsx
+++ frontend/src/frontend/components/TRYMI_login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { API_ENDPOINTS } from "../../config/api";
 import "../styles/login.css";
 
